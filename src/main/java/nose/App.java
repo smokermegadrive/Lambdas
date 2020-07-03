d5658c7e217f46fc78c4148340819e21ef81566c
SmokerMegadrive <agustinpaoletti@protonmail.com>
2020-07-03 20:34:39 -0300

    Hashmap con Variables de objeto

--- src/main/java/nose/App.java
+++ src/main/java/nose/App.java
@@ -10,7 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        Valores valor = new Valores("Eduardo", "Vega");
         /*List<Valores> list = new ArrayList<Valores>();
         list.add(new Valores("Eduardo", "Vega"));
         list.add(new Valores("Jorge", "Garcia"));
@@ -22,21 +21,22 @@ public class App
         .compareTo(y.getApellido()))
         .filter(x -> !x.getApellido().startsWith("P"))
         .forEach(System.out::println);*/
-        Map<Integer, String> map;
+        Map<Integer, Valores> map;
         map = new HashMap<>();
-        map.put(1, "April Murray");
-        map.put(2, "Rose Fisher");
-        map.put(3, "Frank Mitchell");
-        map.put(4, "April Murray");
-        map.put(5, "Charlie Spencer");
-        map.put(6, "Joel Powell");
-        map.put(7, "Adam Blade");
-        map.put(8, "Holly Brooks");
-        map.put(9, "Amy Johnson");
-        map.put(10, "Rebecca Lewis");
-        map.put(11, "Tristan Walker");
-        map.put(12, "Jade Brown");
-        map.put(13, valor.getNombre() + " " + valor.getApellido());
+        map.put(1, new Valores("Roberto", "Garcia"));
+        map.put(2, new Valores("Miguel", "Sanchez"));
+        map.put(3, new Valores("Maria", "Viena"));
+        map.put(4, new Valores("Enesto", "Campo"));
+        map.put(5, new Valores("April", "Murray"));
+        map.put(6, new Valores("Carlos", "Spencer"));
+        map.put(7, new Valores("Joel", "Powell"));
+        map.put(8, new Valores("Adam", "Blade"));
+        map.put(9, new Valores("Amy", "Johnson"));
+        map.put(10, new Valores("Rebecca", "Lewis"));
+        map.put(11, new Valores("Jade", "Brown"));
+        map.put(12, new Valores("Tristan", "Walker"));
+        map.put(13, new Valores("Claire", "Redfield"));
+        /*map.put(13, valor.getNombre() + " " + valor.getApellido());*/
 
         map.entrySet().stream()
         .filter(x -> x.getKey() > 10 || x.getKey() == 8)
